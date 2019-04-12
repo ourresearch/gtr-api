6c5ff78f38d4bcebf372c55fa980de6ff74ce8cf
Heather Piwowar <hpiwowar@gmail.com>
2019-04-12 15:23:50 -0700

    all pictures hack working

--- views.py
+++ views.py
@@ -169,84 +169,71 @@ def get_all_pictures_hack():
         page = int(request.args.get("page"))
     else:
         page = 1
-    if page > 5:
-        abort_json(400, u"Page too large. API currently only supports 5 pages right now, so page must be in the range 0-4.")
 
     if request.args.get("pagesize"):
         pagesize = int(request.args.get("pagesize"))
     else:
         pagesize = 20
-    if pagesize > 20:
-        abort_json(400, u"pagesize too large; max 20")
 
     start_time = time()
 
-    # my_pubs = fulltext_search_title(query)
-    #
-    # print "building response"
-    # sorted_pubs = sorted(my_pubs, key=lambda k: k.adjusted_score, reverse=True)
-    #
-    # my_pub_list = PubList(pubs=sorted_pubs[(pagesize * (page-1)):(pagesize * page)])
-    #
-    # print "getting synonyms"
-    # synonym = get_synonym(query)
-    # print "getting terms from nerd"
-    # term_lookup = get_nerd_term_lookup(query)
-    # if synonym and not term_lookup:
-    #     term_lookup = get_nerd_term_lookup(synonym)
-
     elapsed_time = 0
 
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/4/4a/Impactstory-logo-2014.png"
-    image_uri = "http://en.wikipedia.org/wiki/Extinction"
-    n = 42
-    annotation_title = "Impactstory logo"
+    all_results = []
+    fp = open("entities.tsv", "r")
+    lines = fp.readlines()
+    for line in lines:
+        if line:
+            (image_uri, annotation_title, image_url, n) = line.split("\t")
+            all_results.append(
+                {
+                "abstract": "",
+                "annotations": {},
+                "author_lastnames": [],
+                "best_host": "None",
+                "best_version": "None",
+                "date_of_electronic_publication": "",
+                "doi": "42",
+                "doi_url": "42",
+                "image": {
+                "abstract": "",
+                "confidence": .42,
+                "end": 0,
+                "id": 42,
+                "image_url": image_url,
+                "label": "",
+                "picture_score": 0.42,
+                "raw_top_entity_score": 0.42,
+                "spot": "",
+                "start": 0,
+                "title": "",
+                "types": [],
+                "uri": image_uri,
+                "url": image_url
+                },
+                "is_oa": True,
+                "journal_name": u"n = {}".format(n),
+                "mesh": [],
+                "num_paperbuzz_events": 0,
+                "oa_url": None,
+                "picture_candidates": [],
+                "pmid": 42,
+                "pmid_url": "",
+                "pub_types": [],
+                "score": 0,
+                "short_abstract": None,
+                "snippet": "",
+                "title": annotation_title,
+                "year": None
+                }
+            )
+    fp.close()
 
-    results = [
-            {
-            "abstract": "",
-            "annotations": {},
-            "author_lastnames": [],
-            "best_host": "None",
-            "best_version": "None",
-            "date_of_electronic_publication": "",
-            "doi": "42",
-            "doi_url": "42",
-            "image": {
-            "abstract": "",
-            "confidence": .42,
-            "end": 0,
-            "id": 42,
-            "image_url": image_url,
-            "label": "",
-            "picture_score": 0.42,
-            "raw_top_entity_score": 0.42,
-            "spot": "",
-            "start": 0,
-            "title": "",
-            "types": [],
-            "uri": image_uri,
-            "url": image_url
-            },
-            "is_oa": True,
-            "journal_name": u"n = {}".format(n),
-            "mesh": [],
-            "num_paperbuzz_events": 0,
-            "oa_url": None,
-            "picture_candidates": [],
-            "pmid": 42,
-            "pmid_url": "",
-            "pub_types": [],
-            "score": 0,
-            "short_abstract": None,
-            "snippet": "",
-            "title": annotation_title,
-            "year": None
-            }
-    ]
+    results = all_results[(pagesize * (page-1)):(pagesize * page)]
 
     return jsonify({"results": results,
                     "page": page,
+                    "pagesize": pagesize,
                     "synonym": None,
                     "term_lookup": None,
                     "elapsed_seconds": elapsed_time})
