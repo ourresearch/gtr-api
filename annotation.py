fb51279228c8cbfb2a648c06485ad14f534ab869
Heather Piwowar <hpiwowar@gmail.com>
2019-05-12 11:14:22 -0700

    knock down pictures of urology

--- annotation.py
+++ annotation.py
@@ -176,6 +176,9 @@ class Annotation(object):
         if "sex" in self.title.lower():
             score -= 2
 
+        if "urinart" in self.title.lower() or "urolog" in self.title.lower():
+            score -= 1
+
         if "http://dbpedia.org/ontology/Location" in self.types and self.title not in ["Ancient Egypt"]:
             score += 2
 
