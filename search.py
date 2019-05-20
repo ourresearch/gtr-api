088f1fd9143d5bc91c2e542a2d9bf31cb78d3277
Heather Piwowar <hpiwowar@gmail.com>
2019-05-20 15:37:28 -0700

    tweek scores

--- search.py
+++ search.py
@@ -26,7 +26,7 @@ def adjusted_score(my_dict):
         score -= 5
 
     if my_dict["num_news_events"]:
-        score += math.log10(1 + my_dict.get("num_news_events", 0)) * 5
+        score += math.log10(1 + my_dict.get("num_news_events", 0)) * 4
 
     if my_dict["pub_types"]:
         pub_type_pubmed = my_dict["pub_types"]
